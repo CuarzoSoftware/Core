4aad99b24e691bd17577ed2f361fc5654bfe6003
Eduardo Hopperdietzel <eduardo.hopperdietzel@alumnos.uach.cl>
2025-08-06 23:05:35 -0400

    Rename CZKeyboardModifiers => CZKeyModifiers.

--- src/CZ/Events/CZKeyboardModifiersEvent.h
+++ src/CZ/Events/CZKeyboardModifiersEvent.h
@@ -2,27 +2,7 @@
 #define CZ_CZKEYBOARDMODIFIERSEVENT_H
 
 #include <CZ/Events/CZInputEvent.h>
-
-namespace CZ
-{
-    /**
-     * @brief Keyboard modifiers.
-     */
-    struct CZKeyboardModifiers
-    {
-        /// Active modifiers when physically pressed
-        UInt32 depressed = 0;
-
-        /// Hooked modifiers that will be disabled after a non-modifier key is pressed
-        UInt32 latched = 0;
-
-        /// Active modifiers until they are pressed again (e.g. the Shift key)
-        UInt32 locked = 0;
-
-        /// Group the above states (use this value if the source of a modifier change is not of your interest)
-        UInt32 group = 0;
-    };
-}
+#include <CZ/CZKeyModifiers.h>
 
 /**
  * @brief Keyboard modifiers event.
@@ -34,7 +14,7 @@ class CZ::CZKeyboardModifiersEvent : public CZInputEvent
 public:
     CZ_EVENT_DECLARE_COPY
     CZKeyboardModifiersEvent() noexcept : CZInputEvent(Type::KeyboardModifiers) {};
-    CZKeyboardModifiers modifiers {};
+    CZKeyModifiers modifiers {};
 };
 
 #endif // CZ_CZKEYBOARDMODIFIERSEVENT_H
